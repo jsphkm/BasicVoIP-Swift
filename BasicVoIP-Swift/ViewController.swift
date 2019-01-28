cec9071f5e9ea9635bc601f506e819d400ff0072
jsphkm <jsphkm12@gmail.com>
2019-01-28 15:13:54 -0800

    Initial Commit

---
+++ BasicVoIP-Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BasicVoIP-Swift
+//
+//  Created by Joseph on 1/28/19.
+//  Copyright © 2019 Joseph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
